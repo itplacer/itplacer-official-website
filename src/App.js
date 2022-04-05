27fbc44f142d8ecb6a6033b66a17977f8dec498c
amol dike <amoldike002@gmail.com>
2022-04-05 12:04:21 +0530

    contact form design complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
     <div>
-      <h1>hi</h1>
+      <ContactForm />
     </div>
   );
 }
